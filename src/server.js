7158f573df73ca589992a747109fc57fd68f95dd
arafat-alim <arafat97alim@gmail.com>
2025-04-25 13:34:02 +0530

    ⬆️ PORT name removed from the console log.

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 8811;
 const BASE_URL = process.env.BASE_URL || "http://localhost";
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${BASE_URL}:${PORT}`);
+  console.log(`Server running on ${BASE_URL}`);
 });
